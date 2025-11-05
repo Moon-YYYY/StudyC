996e38bcc7797feb69165a1e1076e799b3d9852c
Moon <yuyang-05@outlook.com>
2025-11-05 08:52:34 +0800

    取余符号更新

--- StudyCpp/studyCpp_base/取余符号/源.cpp
+++ StudyCpp/studyCpp_base/取余符号/源.cpp
@@ -5,5 +5,19 @@ int main() {
 	int a = -9;
 	int b = 2;
 	cout << a % b << endl;//输出-1
+
+	int c = 9;
+	int d = -2;
+	cout << c % d << endl;//输出为1
+
+	//取余符号还适用于判断数字的奇偶性
+	int e = 9, f = 10;
+	cout << e % 2 << endl;//输出1，奇数%2输出1
+	cout << f % 2 << endl;//输出0，偶数%2输出0
+
+	//取余符号还适用于取一个数的最低位
+	int number = 123456;
+	cout << number % 10 << endl;//输出6
+
 	return 0;
 }
