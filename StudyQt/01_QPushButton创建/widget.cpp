a2eeb25e4694b8491b1f49f5de40d3976e26123e
MoonYY05 <yuyang-05@outlook.com>
2026-01-29 16:53:24 +0800

    新增Qt的PushButton创建

---
+++ StudyQt/01_QPushButton创建/widget.cpp
@@ -0,0 +1,25 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include<QPushButton>
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    QPushButton* btn = new QPushButton;
+    btn->setParent(this);
+    btn->setText("first button");
+    QPushButton* btn2 = new QPushButton("second button",this);//按照控件的大小创建窗口
+    //移动btn2
+    btn2->move(100, 100);
+    resize(900, 600);//重置窗口大小
+    //设置窗口标题
+    setWindowTitle("第一个窗口");
+    //设置固定窗口大小
+    setFixedSize(900, 600);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
